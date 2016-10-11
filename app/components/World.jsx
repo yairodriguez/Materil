7d9f04d0379e3649faf55aa8479fdb0385406371
Yair Rodriguez <rodriguezortega.yair@gmail.com>
2016-10-10 22:40:27 -0500

    [component] Add constructor to World component.

--- app/components/World.jsx
+++ app/components/World.jsx
@@ -1,4 +1,17 @@
 import React from 'react';
 import Hello from './Hello';
 
-export default class World extends React.Component {}
+export default class World extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      greet: props.greet,
+      value: 'Yair'
+    };
+
+    this.slangGreet = this.slangGreet.bind(this);
+    this.slangHindi = this.slangHindi.bind(this);
+    this.handleNameChange = this.handleNameChange.bind(this);
+  }
+}
